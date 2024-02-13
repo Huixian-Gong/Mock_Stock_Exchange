1082b666ece1555d4ed0807dcf10578c2a187cd4
Huixian-Gong <huixian@usc.edu>
2024-02-13 00:44:42 -0800

    added hw2

---
+++ Assignment_2/main.py
@@ -0,0 +1,70 @@
+'''
+Finhub API key: cms4589r01qlk9b11g1gcms4589r01qlk9b11g20
+Polygon.io API key: wjYgWwDJ_fIrb0Iem90No7WkqTF1yyYu
+'''
+
+from flask import Flask, jsonify, request
+from datetime import *
+from dateutil.relativedelta import *
+import finnhub, requests, calendar
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def homepage():
+    return app.send_static_file("index.html")
+
+@app.route('/lookup')
+def lookup():
+    symbol = request.args.get('symbol')
+    api_key="cms4589r01qlk9b11g1gcms4589r01qlk9b11g20"
+    url = f'https://finnhub.io/api/v1/stock/profile2?symbol={symbol}&token={api_key}'
+    response = requests.get(url)
+    return jsonify(response.json())
+
+@app.route('/stock')
+def stock():
+    symbol = request.args.get('symbol')
+    api_key="cms4589r01qlk9b11g1gcms4589r01qlk9b11g20"
+    quote_url = f'https://finnhub.io/api/v1/quote?symbol={symbol}&token={api_key}'
+    rec_url = f'https://finnhub.io/api/v1/stock/recommendation?symbol={symbol}&token={api_key}'
+    quote_response = requests.get(quote_url)
+    rec_response = requests.get(rec_url)
+
+    ret = (quote_response.json())|((rec_response.json())[0])
+    # print(ret)
+    return jsonify(ret)
+
+@app.route('/chart')
+def chart():
+    symbol = request.args.get('symbol').upper()
+    multiplier = 1
+    timespan = 'day'
+    end_date = date.today()
+    # end_date = end_date+relativedelta(days=-19)
+    print(end_date)
+    from_date = end_date+relativedelta(months=-6, days=-1)
+    api_key="wjYgWwDJ_fIrb0Iem90No7WkqTF1yyYu"
+    url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/{multiplier}/{timespan}/{from_date}/{end_date}?adjusted=true&sort=asc&apiKey={api_key}'
+    response = requests.get(url)
+    data = []
+    for i in response.json()['results']:
+        data.append([i['t'], i['c'],i['v']])
+    # print(url)
+    return data
+
+
+@app.route('/news')
+def news():
+    symbol = request.args.get('symbol')
+    today_date = end_date = date.today()
+    before_30 = end_date+relativedelta(days=-30)
+    api_key="cms4589r01qlk9b11g1gcms4589r01qlk9b11g20"
+    url = f'https://finnhub.io/api/v1/company-news?symbol={symbol}&from={before_30}&to={today_date}&token={api_key}'
+    response = requests.get(url)
+    # print(url)
+    # print(jsonify(response.json()))
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+     app.run(debug=True)
---
+++ Assignment_2/static/js/script.js
@@ -0,0 +1,321 @@
+document.addEventListener("DOMContentLoaded",  function() {
+
+    document.getElementById("stockSearchForm").addEventListener("submit", async function(e) {
+        e.preventDefault(); // Prevent form from submitting normally
+        const ticker = document.getElementById("ticker").value.toUpperCase();
+        fetch(`/lookup?symbol=${ticker}`)
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("results").innerHTML = "";
+                if (Object.keys(data).length === 0 && data.constructor === Object) {
+                    document.getElementById("results").innerHTML = "<p class=\"empty_result\"> <span>Error: No record has been found, please enter a valid symbol</span></p>";
+                } else {
+                    document.getElementById("results").innerHTML =
+                        "<div class=\"tabs\"><button id=\"company_summary\">Company</button>"+
+                        "<button id=\"stock_summary\">Stock Summary</button>"+
+                        "<button id=\"charts\">Charts</button>"+
+                        "<button id=\"latest_news\">Latest News</button></div>"+
+                        "<div id=\"tabs_below\"></div>" ;
+                    tabs_company(data);
+                }
+            })
+            .catch(error => console.error('Error:', error));
+            
+
+        const urls = [`/lookup?symbol=${ticker}`,`/stock?symbol=${ticker}`, `/chart?symbol=${ticker}`, `/news?symbol=${ticker}`];
+        const ret = await async_fetch(urls);
+        ticker_result(ret, ticker);
+        console.log('done');
+
+    });
+
+    document.getElementById("clearButton").addEventListener("click", function() {
+        document.getElementById("ticker").value = ''; 
+        document.getElementById("results").innerHTML = ''; 
+    });
+
+});
+
+
+async function async_fetch(urls) {
+
+    const promises = urls.map(url => fetch(url));
+
+    try {
+        const responses = await Promise.all(promises);
+        const data = await Promise.all(responses.map(response => response.json()));
+        return data; 
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error; 
+    }
+}
+
+function ticker_result(ret,ticker) {
+    document.getElementById("company_summary").addEventListener("click", function() {
+        // console.log(ret);
+        tabs_company(ret[0]);
+    });
+    document.getElementById("stock_summary").addEventListener("click", function() {
+        func_stock_summary(ret[1]);
+    });
+    document.getElementById("charts").addEventListener("click", function() {
+        func_charts(ret[2],ticker);
+    });
+    document.getElementById("latest_news").addEventListener("click", function() {
+        func_latest_news(ret[3]);
+    });
+
+}
+
+function tabs_company(data) {
+    document.getElementById("tabs_below").innerHTML= "<img class=\"logos\" src='" + data.logo +" '>"+
+    "<table class=\"company\">"+
+    "<tr><td class=\"company_left\">Company Name</td><td class=\"company_right\">"+data.name+ "</td>"+
+    "<tr><td class=\"company_left\">Stock Ticker Symbol</td><td class=\"company_right\">"+data.ticker+ "</td>"+
+    "<tr><td class=\"company_left\">Stock Exchange Code</td><td class=\"company_right\">"+data.exchange+ "</td>"+
+    "<tr><td class=\"company_left\">Company Start Date</td><td class=\"company_right\">"+data.ipo+ "</td>"+
+    "<tr><td class=\"company_left\">Category</td><td class=\"company_right\">"+data.finnhubIndustry+ "</td>"+"</table>";
+    document.getElementById("company_summary").style.backgroundColor="rgb(204, 204, 204)";
+    document.getElementById("stock_summary").style.removeProperty("background-color");
+    document.getElementById("charts").style.removeProperty("background-color");
+    document.getElementById("latest_news").style.removeProperty("background-color");
+    
+    
+}
+
+function func_stock_summary(data) {
+
+    document.getElementById("tabs_below").innerHTML="<table class=\"stock\">"+
+    "<tr><td class=\"company_left\">Stock Ticker Symbol</td><td class=\"company_right\">"+data.symbol+"</td>"+
+    "<tr><td class=\"company_left\">Trading Day</td><td class=\"company_right\">"+ trading_day(data.t)+"</td>"+ 
+    "<tr><td class=\"company_left\">Previous Closing Price</td><td class=\"company_right\">"+data.pc+"</td>"+
+    "<tr><td class=\"company_left\">Opening Price</td><td class=\"company_right\">"+data.o+"</td>"+
+    "<tr><td class=\"company_left\">High Price</td><td class=\"company_right\">"+data.h+"</td>"+
+    "<tr><td class=\"company_left\">Low Price</td><td class=\"company_right\">"+data.l+"</td>"+
+    "<tr><td class=\"company_left\">Change</td><td class=\"company_right\">"+trading_change(data.d)+"</td>"+
+    "<tr><td class=\"company_left\">Change Percent</td><td class=\"company_right\">"+trading_change(data.dp)+"</td>"+
+    "</table>"+
+    "<p id=\"rec_trend\"><span class=\"left_end\">Strong</br>Sell</span>"+
+    "<span class=\"color_num color_box1\">"+ data.strongSell +"</span>"+
+    "<span class=\"color_num color_box2\">"+ data.sell +"</span>"+
+    "<span class=\"color_num color_box3\">"+ data.hold +"</span>"+
+    "<span class=\"color_num color_box4\">"+ data.buy +"</span>"+
+    "<span class=\"color_num color_box5\">"+ data.strongBuy +"</span>"+
+    "<span class=\"right_end\">Strong</br>Buy</span>"+"</p><p class=\"rec_trend\">Recommendation Trends</p>"
+
+    document.getElementById("company_summary").style.removeProperty("background-color");
+    document.getElementById("stock_summary").style.backgroundColor="rgb(204, 204, 204)";
+    document.getElementById("charts").style.removeProperty("background-color");
+    document.getElementById("latest_news").style.removeProperty("background-color");
+
+}
+
+function trading_day(time_e) {
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    t = new Date();
+    // console.log(t.getDate())
+    t = t.getDate() + " " + month[t.getMonth()] + ", " + t.getFullYear();
+    return t;
+}
+
+function trading_change(val) {
+    if (val > 0) {
+        return val + "<img class=\"arrows\" src=\"/static/img/GreenArrowUp.png\">";
+    } else if (val < 0) {
+        return val + "<img class=\"arrows\" src='/static/img/RedArrowDown.png'>";
+    } else {
+        return val;
+    }
+}
+
+function func_charts(rawData,ticker) {
+
+    document.getElementById("tabs_below").innerHTML="<div id=\"plot\"></div>";
+    const priceData = rawData.map(item => [item[0], item[1]]);
+    const volumeData = rawData.map(item => [item[0], item[2]]);
+    // console.log(volumeData)
+    // const max_v = Math.max(volumeData.)
+    
+    const secondElements = volumeData.map(item => item[1]); // Extract the second elements
+    const maxSecondElement = Math.max(...secondElements); // Find the max value
+
+    Highcharts.setOptions({
+        lang: {
+            rangeSelectorZoom: 'Zoom'
+        }
+    });
+
+    Highcharts.stockChart('plot', {
+
+        rangeSelector: {
+            selected: 4,
+            inputEnabled: false,
+            buttons: [{
+                type: 'day',
+                count: 7,
+                text: '7d',
+            },{
+                type: 'day',
+                count: 15,
+                text: '15d',
+            },{
+                type: 'month',
+                count: 1,
+                text: '1m',
+            },{
+                type: 'month',
+                count: 3,
+                text: '3m',
+            },{
+                type: 'month',
+                count: 6,
+                text: '6m',
+            }],
+        },
+
+        title: {
+            text: ' Stock Price ' + ticker + ' ' + today_date(),
+        },
+
+        caption: {
+            useHTML: true, 
+            text: '<a href="https://polygon.io/" target="_blank"> Source: polygon.io</a>',
+            align: 'center', 
+            verticalAlign: 'top' 
+        },
+
+        navigator: {
+            series: {
+                accessibility: {
+                    exposeAsGroupOnly: true
+                }
+            }
+        },
+        xAxis: {
+            type: 'datetime',
+            min: volumeData[0][0],  // Assuming volumeData is sorted by date
+            max: volumeData[volumeData.length - 1][0],
+            endOnTick: false,
+            startOnTick: false,
+            showLastLabel: true  // Ensure the last label is shown
+        },
+        yAxis: [{
+            labels:{
+                align: 'left',
+                x: -30,
+            },
+            title: {
+                text: 'Stock Price',
+                offset: 40,
+            },
+            height: '100%',
+            opposite: false
+        },{
+            labels:{
+                align: 'left',
+                x:30
+            },
+            title: {
+                text: 'Volumn',
+                offset: 80
+            },
+            height: '100%',
+            pointRange: 24 * 3600 * 1000,
+            max: 2 * maxSecondElement
+        }],
+
+        series: [{
+            name: 'Stock Price',
+            data: priceData,
+            type: 'area',
+            threshold: null,
+            tooltip: {
+                valueDecimals: 2
+            },
+            fillColor: {
+                linearGradient: {
+                    x1: 0,
+                    y1: 0,
+                    x2: 0,
+                    y2: 1
+                },
+                stops: [
+                    [0, Highcharts.getOptions().colors[0]],
+                    [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                ]
+            }
+        },{
+            type: 'column',
+            name: 'Volumn',
+            data: volumeData,
+            yAxis: 1,
+            color: 'black',
+        }],
+        plotOptions: {
+
+            column: {
+                pointWidth: 5, // Adjust the width of the volume bars
+                pointPadding: 0,
+                groupPadding: 0.1, // Use a small value to have some space between bars
+                pointPlacement: 'on' // Place on the ticks, may need adjustment based on data
+            },
+        }
+    })
+
+    document.getElementById("company_summary").style.removeProperty("background-color");
+    document.getElementById("stock_summary").style.removeProperty("background-color");
+    document.getElementById("charts").style.backgroundColor="rgb(204, 204, 204)";
+    document.getElementById("latest_news").style.removeProperty("background-color");
+}
+
+function func_latest_news(data) {
+
+    document.getElementById("tabs_below").innerHTML = show_news(data);
+
+    document.getElementById("company_summary").style.removeProperty("background-color");
+    document.getElementById("stock_summary").style.removeProperty("background-color");
+    document.getElementById("charts").style.removeProperty("background-color");
+    document.getElementById("latest_news").style.backgroundColor="rgb(204, 204, 204)";
+    
+}
+
+function show_news(data) {
+    const filteredData = data.filter(item => 
+        item.datetime && item.image && item.url && item.headline).slice(0, 5);
+    ret_html = ""
+    for (i = 0; i < Math.min(5, data.length); i+=1) {
+        ret_html += "<div class=\"news\"> <img class=\"news_img\" src='"+filteredData[i].image+"'>"+ "<div class=\"news_text\"><span class=\"heading\">"+filteredData[i].headline+"</span>"+
+            "<span class=\"content\">" +trading_day(filteredData[i].datetime)+ "</span><a class=\"content\" href=\""+filteredData[i].url+"\" target=\"_blank\">See Original Post</a></div></div>"
+    }
+
+    return ret_html
+}
+
+function today_date() {
+    d = new Date();
+    date = "0";
+    const month = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    if (d.getDate() <10) {
+        date += String(d.getDate())
+    } else {
+        date = d.getDate()
+    }
+    return d.getFullYear() + "-" + month[d.getMonth()] + "-" + date; 
+}
+
+function before_30() {
+    d = new Date();
+    date = "0";
+    d.setDate(d.getDate() - 30);
+    const month = ["01","02","03","04","05","06","07","08","09","10","11","12"];
+    if (d.getDate() <10) {
+        date += String(d.getDate())
+    } else {
+        date = d.getDate()
+    }
+    return d.getFullYear() + "-" + month[d.getMonth()] + "-" + date; 
+}
+
+
+
